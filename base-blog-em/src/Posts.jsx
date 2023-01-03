8ca2a5ad00f10897cc183e00443e4aef4a9f83f4
Steven Hill <steven@stevenhill.co.za>
2023-01-03 17:57:08 +0200

    manual component pagination

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -4,22 +4,26 @@ import { useQuery } from '@tanstack/react-query';
 import { PostDetail } from './PostDetail';
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    'https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0'
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // Destructure the data property from the returned useQuery object
   // The useQuery object returns with a lot of properties we can use
   // useQuery args: 1st arg: The QUERY KEY i.e. name of the query; 2nd arg: function to get the data e.g. using native fetch or axios
   // Remember fetching data from the server is asynchronous, so we have to handle the waiting period, otherwise WILL get an error
-  const { data, isError, error, isLoading } = useQuery(['posts'], fetchPosts);
+  const { data, isError, error, isLoading } = useQuery(
+    ['posts', currentPage],
+    () => fetchPosts(currentPage)
+  );
+
   if (isLoading) return <h3>Loading...</h3>;
 
   if (isError)
@@ -44,11 +48,21 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((prevValue) => prevValue - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((prevValue) => prevValue + 1);
+          }}
+        >
           Next page
         </button>
       </div>
