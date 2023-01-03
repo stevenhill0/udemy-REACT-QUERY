23cf38e608db9c644ffaf945553e60da6320dabf
Steven Hill <steven@stevenhill.co.za>
2023-01-03 15:40:30 +0200

    Beginner project

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,13 +1,24 @@
-import { Posts } from "./Posts";
-import "./App.css";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import { Posts } from './Posts';
+import './App.css';
+
+// The query client manages the queries and the cache
+// It contains the cache all the default options
+// The Provider takes the client as a prop
+const queryClient = new QueryClient();
 
 function App() {
   return (
     // provide React Query client to App
-    <div className="App">
-      <h1>Blog Posts</h1>
-      <Posts />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Blog Posts</h1>
+        <Posts />
+      </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
-import { PostDetail } from "./PostDetail";
+import { PostDetail } from './PostDetail';
 const maxPostPage = 10;
 
 async function fetchPosts() {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    'https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0'
   );
   return response.json();
 }
@@ -14,8 +15,20 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  // Destructure the data property from the returned useQuery object
+  // The useQuery object returns with a lot of properties we can use
+  // useQuery args: 1st arg: The QUERY KEY i.e. name of the query; 2nd arg: function to get the data e.g. using native fetch or axios
+  // Remember fetching data from the server is asynchronous, so we have to handle the waiting period, otherwise WILL get an error
+  const { data, isError, error, isLoading } = useQuery(['posts'], fetchPosts);
+  if (isLoading) return <h3>Loading...</h3>;
+
+  if (isError)
+    return (
+      <>
+        <h3>Oops something went wrong</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
 
   return (
     <>
