7069993f2356c247ff8a430e6dd929ad61e24c30
Steven Hill <steven@stevenhill.co.za>
2023-01-13 11:49:57 +0200

    setup prefetch custom hook

--- base-lazy-days/client/src/components/app/Home.tsx
+++ base-lazy-days/client/src/components/app/Home.tsx
@@ -3,8 +3,13 @@ import { ReactElement } from 'react';
 import { GiFlowerPot } from 'react-icons/gi';
 
 import { BackgroundImage } from '../common/BackgroundImage';
+import { usePrefetchTreatments } from '../treatments/hooks/useTreatments';
 
 export function Home(): ReactElement {
+  // Calling usePrefetchTreatments in the Home component cos the Home component will NOT be called often
+  // Also cos usePrefetchTreatments is a hook we CANT use a useEffect, and we cant make  usePrefetchTreatments into normal function cos otherwise won't be able to use the useQueryClient hook within it
+  usePrefetchTreatments();
+
   return (
     <Stack align="center" justify="center" height="84vh">
       <BackgroundImage />
--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,15 +1,27 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
 
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get('/treatments');
+  return data;
+}
+
+// Using an empty array as a fallback UNTIL ALL data is fetched from the server via the getTreatments function
 export function useTreatments(): Treatment[] {
-  // TODO: get data from server via useQuery
-  return [];
+  const fallback = [];
+  const { data = fallback } = useQuery([queryKeys.treatments], getTreatments);
+
+  return data;
+}
+// not going to return anything, why we have void. Its purpose is to populate the cache
+export function usePrefetchTreatments(): void {
+  // prefetchQuery 1st arg: key. The key must MATCH the relevant useQuery key, for the data you want to cache. In this case the key from the useTreatments hook
+  // prefetchQuery 2nd arg: function. We are using the same getTreatments function from useTreatments
+  const queryClient = useQueryClient();
+  queryClient.prefetchQuery([queryKeys.treatments], getTreatments);
 }
