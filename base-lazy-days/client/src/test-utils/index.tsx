833e0d7db976e9c7b5aba5d4f8ddfda0c3affdbb
Steven Hill <steven@stevenhill.co.za>
2023-01-26 21:14:15 +0200

    Setup 1st query test

--- base-lazy-days/client/src/test-utils/index.tsx
+++ base-lazy-days/client/src/test-utils/index.tsx
@@ -1,6 +1,24 @@
-import { render } from '@testing-library/react';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { render, RenderResult } from '@testing-library/react';
+import { ReactElement } from 'react';
 // import { defaultQueryClientOptions } from '../react-query/queryClient';
+
+// function to create a unique query client for each test
+const generateQueryCLient = () => {
+  return new QueryClient();
+};
+
+// If the client was provided we use it, but if NO client we provide it with a new one
+export const renderWithQueryClient = (
+  ui: ReactElement,
+  client?: QueryClient,
+): RenderResult => {
+  const queryClient = client ?? generateQueryCLient();
+
+  return render(
+    <QueryClientProvider client={queryClient}>{ui}</QueryClientProvider>,
+  );
+};
 
 // from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
 // export const createQueryClientWrapper = (): React.FC => {
